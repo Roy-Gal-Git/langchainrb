f8efb1ae93b187e62021235a1564f84da5124ffa
Roy Gal <roy.gal@biodata.com>
2025-12-18 23:38:23 +0200

    refactor(assistant): update adapter to support AwsBedrockConverse

--- lib/langchain/assistant/llm/adapter.rb
+++ lib/langchain/assistant/llm/adapter.rb
@@ -8,7 +8,7 @@ module Langchain
         def self.build(llm)
           if llm.is_a?(Langchain::LLM::Anthropic)
             LLM::Adapters::Anthropic.new
-          elsif llm.is_a?(Langchain::LLM::AwsBedrock) && llm.defaults[:chat_model].include?("anthropic")
+          elsif (llm.is_a?(Langchain::LLM::AwsBedrockConverse) || (llm.is_a?(Langchain::LLM::AwsBedrock)) && llm.defaults[:chat_model].include?("anthropic"))
             LLM::Adapters::AwsBedrockAnthropic.new
           elsif llm.is_a?(Langchain::LLM::GoogleGemini) || llm.is_a?(Langchain::LLM::GoogleVertexAI)
             LLM::Adapters::GoogleGemini.new
